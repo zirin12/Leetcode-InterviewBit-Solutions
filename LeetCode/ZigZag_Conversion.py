9a2069a33234b4236178cf041b577449a9d4d8ee
zirin12 <20094275+zirin12@users.noreply.github.com>
2020-04-09 00:27:33 +0530

    Update ZigZag_Conversion.py

--- LeetCode/ZigZag_Conversion.py
+++ LeetCode/ZigZag_Conversion.py
@@ -18,7 +18,8 @@ Explanation:
 class Solution:
     # Find relation between diagonal elements and it's previous element in the zig zag representation
     # Code looks messy because of extra conditions to take care of special cases
-    def convert(self, s: str, numRows: int) -> str:
+    # This can be simplified by finding out the formula for getting each element in the row and then simplifying relations
+    def convert_1(self, s: str, numRows: int) -> str:
         if(numRows > len(s)):
             return s
         output = ""
@@ -35,3 +36,29 @@ class Solution:
                     output  = (output + s[inner_index]) if inner_index < len(s) else output         
             factor+=1
         return output
+    
+    # simplest way ,decide the row each character belongs to by traversing characters in s with simple row variable tracker
+    # and direction 
+    def convert_2(self, s: str, numRows: int) -> str:
+        if numRows == 1 :
+            return s
+        numRows = min(numRows,len(s))
+        list_rows = [""]*numRows
+        going_down = True
+        curr_row = 0
+        for char in s:
+            list_rows[curr_row]+=char
+            if going_down:
+                curr_row+=1
+            else:
+                curr_row-=1
+            
+            if curr_row==0:
+                going_down = True
+            elif curr_row==numRows-1:
+                going_down=False
+            
+        result = ""
+        for string in list_rows:
+            result+=string
+        return result
