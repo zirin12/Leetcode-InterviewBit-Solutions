72e3262c8f75aa44cb10212e3bda25e53b2b2de9
zirin12 <20094275+zirin12@users.noreply.github.com>
2020-06-10 18:34:59 +0530

    Added other possible solutions and a final comment

--- Hackerearth/knapsack.py
+++ Hackerearth/knapsack.py
@@ -1,10 +1,5 @@
 '''
-# Sample code to perform I/O:
 
-name = input()                  # Reading input from STDIN
-print('Hi, %s.' % name)         # Writing output to STDOUT
-
-# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
 '''
 
 # Write your code here
@@ -61,3 +56,64 @@ for i in range(1,N):
             memo[j] = max(v[i] + memo[j-w[i]],memo[j])
 
 print(memo[W])
+
+#Another Solution
+***************************************************************************************************************
+N,W = list(map(int,input().split()))
+v = list(map(int,input().split()))
+w = list(map(int,input().split()))
+
+memo = [[0 for i in range(50000+1)] for j in range(N)]
+
+# Dp solution using values and indexes as states , in this case take minimum weight of item included and item not included
+memo[0][v[0]] = w[0]
+for i in range(1,N):
+    for j in range(50000+1):
+        if j == v[i] :
+            memo[i][j] = w[i]
+        if memo[i-1][j]:
+            memo[i][j] = memo[i-1][j]
+        if j > v[i] and memo[i-1][j-v[i]] :
+            if memo[i-1][j] :
+                memo[i][j] = min(w[i] + memo[i-1][j-v[i]],memo[i-1][j])
+            else :
+                memo[i][j] = w[i] + memo[i-1][j-v[i]]
+        
+for j in range(50000,0,-1):
+    if memo[N-1][j] != 0 and memo[N-1][j] <= W:
+        print(j)
+        break
+
+
+# Another Solution
+******************************************************************************************************
+N,W = list(map(int,input().split()))
+v = list(map(int,input().split()))
+w = list(map(int,input().split()))
+
+memo = [0 for i in range(50000+1)]
+
+# Simplification of the above solution to 1d array and starting the inner loop backwards 
+# Still doesn't pass all test cases , some of them time out
+# Have to choose which state to choose , weight or value against index 
+# This choice should be done to see if W > 50000
+memo[v[0]] = w[0]
+for i in range(1,N):
+    for j in range(50000,0,-1):
+        if j < v[i]:
+            break
+        if j == v[i] :
+            if memo[j]:
+                memo[j] = min(w[i],memo[j])
+            else :
+                memo[j] = w[i]
+        if j > v[i] and memo[j-v[i]] :
+            if memo[j] :
+                memo[j] = min(w[i] + memo[j-v[i]],memo[j])
+            else :
+                memo[j] = w[i] + memo[j-v[i]]
+        
+for j in range(50000,0,-1):
+    if memo[j] != 0 and memo[j] <= W:
+        print(j)
+        break
